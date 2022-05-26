5e797574aeeaad9d7b3ab45e1f0f01d81b9e4fa4
arflint48 <arflint@go.olemiss.edu>
2022-05-26 15:44:56 -0500

    new code

--- abbreviate.py
+++ abbreviate.py
@@ -1,8 +1,17 @@
-#take updated refs.bib file and create one with abbreviated journals
+#take updated refs.bib file and create one with abbreviated journal names abbrevrefs.bib
 
 import sys, os, re
-import pandas as pd
 
-bib = open("refs.bib", "r") #refs.bib calls the bibtex file
+bib = open("refs.bib", "r").read() #refs.bib calls the bibtex file
+journallist = open("journallist.txt")
 
+for journal in journallist.readlines()[::-1]:
+    full, abbrev = journal.strip().split(" = ")
+    if full != full.upper() and (" " in full):
+        bib = bib.replace(full.strip(), abbrev.strip())
+
+with open("abbrevrefs.bib", "w") as abbrevbib: #file the modified bibliography will be written to
+    abbrevbib.write(bib)
+
+abbrevbib.close()
 bib.close()
