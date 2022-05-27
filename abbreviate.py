efc1376a98dfc28e040abacbf70e326205801bf1
arflint48 <arflint@go.olemiss.edu>
2022-05-26 21:45:00 -0500

    code done

--- abbreviate.py
+++ abbreviate.py
@@ -3,13 +3,13 @@
 import sys, os, re
 
 bib = open("refs.bib", "r").read() #refs.bib calls the bibtex file
-journallist = open("journallist.txt")
+journallist = open("journallist.txt") #calls the list of journals and their abbreviations
 
-for journal in journallist.readlines()[::-1]:
-    full, abbrev = journal.strip().split(" = ")
+for journal in journallist.readlines(): #reads in each journal individually
+    full, abbrev = journal.strip().split(" = ") #creates journal/abbreviation pair
+    full = ("{" + full + "}") #forces the journal name to be a full match rather than a partial one
     if full != full.upper() and (" " in full):
-        bib = bib.replace(full.strip(), abbrev.strip())
-#bib = bib.replace("journal = {International", "journal = {Int.")
+        bib = bib.replace(full.strip(),("{" + abbrev.strip() + "}")) #replaces the journal name with the abbreviation and restores bibtex formatting
 
 with open("abbrevrefs.bib", "w") as abbrevbib: #file the modified bibliography will be written to
     abbrevbib.write(bib)
