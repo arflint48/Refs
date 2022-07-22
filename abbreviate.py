f9ae022c6b4c7c705ca501ed124441d27b30c59c
arflint48 <arflint@go.olemiss.edu>
2022-07-22 11:16:54 -0500

    cite mode apj

--- abbreviate.py
+++ abbreviate.py
@@ -6,13 +6,13 @@ bib = open("refs.bib", "r").read() #refs.bib calls the bibtex file
 journallist = open("journallist.txt") #calls the list of journals and their abbreviations
 
 #for ApJ and related journals only:
-#apjlist = open("apjlist.txt")
+apjlist = open("apjlist.txt")
 
-#for apjref in apjlist.readlines():
-    #full, abbrev = apjref.strip().split(" = ")
-    #full = ("{" + full + "}")
-    #if full != full.upper():
-        #bib = bib.replace(full.strip(),("{" + abbrev.strip() + "}"))
+for apjref in apjlist.readlines():
+    full, abbrev = apjref.strip().split(" = ")
+    full = ("{" + full + "}")
+    if full != full.upper():
+        bib = bib.replace(full.strip(),("{" + abbrev.strip() + "}"))
 
 for journal in journallist.readlines(): #reads in each journal individually
     full, abbrev = journal.strip().split(" = ") #creates journal/abbreviation pair
